0006acf547b69981ba43fcf5d2dd6b5fae4e7c53
lelismo <eusouoleli@gmail.com>
2022-02-21 04:38:13 -0300

    ex015

--- ex014/script.js
+++ ex014/script.js
@@ -1,20 +1,20 @@
 function carregar() {
-var msg = document.getElementById('msg')
-var img = document.getElementById('imagem')
-var data = new Date()
-var hora = data.getHours()
-msg.innerHTML = `Agora são ${hora} horas`
-if (hora >= 0 && hora < 12) {
-    // BOM DIA
-    img.src = 'imagens/manha-redonda.png'
-    document.body.style.backgroundColor = '#F1D5B0'
-} else if (hora >= 12 && hora < 18) {
-    // BOA TARDE
-    img.src = 'imagens/tarde-redonda.png'
-    document.body.style.backgroundColor = '#FAA243'
-} else {
-    // BOA NOITE
-    img.src = 'imagens/noite-redonda.png'
-    document.body.style.backgroundColor = '#4D3A58'
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12) {
+        // BOM DIA
+        img.src = 'imagens/manha-redonda.png'
+        document.body.style.backgroundColor = '#F1D5B0'
+    } else if (hora >= 12 && hora < 18) {
+        // BOA TARDE
+        img.src = 'imagens/tarde-redonda.png'
+        document.body.style.backgroundColor = '#FAA243'
+    } else {
+        // BOA NOITE
+        img.src = 'imagens/noite-redonda.png'
+        document.body.style.backgroundColor = '#4D3A58'
 }
 }
--- ex015/script.js
+++ ex015/script.js
@@ -2,8 +2,47 @@ function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
-    var res = document.querySelector('div#res')
+    var res = document.getElementById('res')
     if (fano.value.length == 0 || Number(fano.value) > ano) {
         alert('[ERRO] Verifique os dados e tente novamente')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10) {  
+                // kid
+                img.setAttribute('src', 'imagens/criancahp.png')
+            } else if (idade < 30) {
+                // jovem
+                img.setAttribute('src', 'imagens/jovemhp.png')
+            } else if (idade < 60) {
+                // adulto
+                img.setAttribute('src', 'imagens/adultohp.png')
+            } else {
+                // idoso
+                img.setAttribute('src', 'imagens/idosohp.png')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {  
+                // kid
+                img.setAttribute('src', 'imagens/criancamp.png')
+            } else if (idade < 30) {
+                // jovem
+                img.setAttribute('src', 'imagens/jovemmp.png')
+            } else if (idade < 60) {
+                // adulto
+                img.setAttribute('src', 'imagens/adultomp.png')
+            } else {
+                // idoso
+                img.setAttribute('src', 'imagens/idosomp.png')
+            }
+        }
+        res.innerHTML = `${genero} com ${idade} anos`
+        res.appendChild(img)
     }
 }
